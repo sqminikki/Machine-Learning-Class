25688d9520897d3ac0e390654c0d6b356d571de1
sqminikki <ssammie47@gmail.com>
2026-02-06 21:03:18 -0600

    commenting

--- Assignment 1/datapreparation.py
+++ Assignment 1/datapreparation.py
@@ -1,2 +1,7 @@
 import numpy as np #This is just NumPy. It will making writing this type of code much easier.
 from tensorflow.keras.datasets.mnist import mnist
+
+# Extract digits 0, 1, and 9 ONLY.
+
+# Vectorize each image into a column vector of length 784 and stack all vectors as columns of the data matrix 
+# where N is the total number of selected images. Set the data type of X to float32.
