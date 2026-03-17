728c6c7b203b0fccf51d826ef6a99359bce3c71c
cristinot3 <cristino.torruella@my.utsa.edu>
2026-03-17 18:51:06 -0500

    problem 3 completed

--- Homework 2/problem3.py
+++ Homework 2/problem3.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = np.loadtxt("HW2_A.csv")
-y = np.loadtxt("HW2_y.csv")
-x0 = np.loadtxt("HW2_xinit.csv")
+A = np.loadtxt("S2026_HW2_CSV_DATA/HW2_CSV_DATA/HW2_A.csv")
+y = np.loadtxt("S2026_HW2_CSV_DATA/HW2_CSV_DATA/HW2_y.csv")
+x0 = np.loadtxt("S2026_HW2_CSV_DATA/HW2_CSV_DATA/HW2_xinit.csv")
 
 def L(x):
     r = y - A @ x
@@ -39,23 +39,23 @@ print("C =", C)
 print("1/C =", invC)
 print("L(x_30) =", L_vals[-1])
 
-# Fig. 1
+# --- Task 1: Plot L(x_n) vs iteration index Figure 1 ---
 iters = np.arange(1, 31)
 plt.figure()
 plt.plot(iters, L_vals, 'b-', label=r'$L(x_n)$')
 plt.axhline(L_opt, color='k', linestyle='--', label=r'$L_{\mathrm{opt}}$')
-plt.xlabel("iteration n")
+plt.xlabel("iteration index n")
 plt.ylabel(r"$L(x_n)$")
 plt.legend()
 plt.tight_layout()
-plt.show()
+plt.savefig("fig1_gd_els_L(x_n).png")
 
-# Fig. 2
+# --- Task 3: Plot L(x_n) vs iteration index Figure 1 ---
 plt.figure()
 plt.plot(iters, gammas, 'b-', label=r'$\gamma_n$')
 plt.axhline(invC, color='k', linestyle='--', label=r'$1/C$')
-plt.xlabel("iteration n")
+plt.xlabel("iteration index n")
 plt.ylabel(r'$\gamma_n$')
 plt.legend()
 plt.tight_layout()
-plt.show()
+plt.savefig("fig2_gd_els_gamma.png")
